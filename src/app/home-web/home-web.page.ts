834f532380ea44f818b29a11c35d1d2a2af38818
SupaGanon <163149324+SupaGanon@users.noreply.github.com>
2024-11-22 21:34:32 -0700

    adding auth method

--- src/app/home-web/home-web.page.ts
+++ src/app/home-web/home-web.page.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-home-web',
@@ -6,9 +10,41 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./home-web.page.scss'],
 })
 export class HomeWebPage implements OnInit {
-  constructor() { }
+  Nombre: string;
+  status: string;
+  rol: string;
+  tokenUser: string;
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cookieService: CookieService
+  ) { }
+
+  ngOnInit(): void {
+    const token = this.cookieService.get('token');
+    console.log(token)
+    if (!token) {
+      this.router.navigate(['']);
+      return;
+    }
+  
+    this.authService.user().subscribe(user => {
+      if (user && user.user_info) { // Verificamos que user y user.user_info estén definidos
+        this.rol = user.user_info.rol; // Asignamos el valor de rol correctamente
+        this.tokenUser = user.token;
+        console.log(this.rol);
+  
+        if (this.rol !== 'Admin' && this.tokenUser === "") { // Verificamos el valor de rol correctamente
+          this.router.navigate(['']);
+        }
+      } else {
+        console.error('user_info no está definido', user ? user.user_info : 'user es undefined');
+        this.router.navigate(['']);
+      }
+    }, error => {
+      console.error('Error obteniendo la información del usuario:', error);
+      this.router.navigate(['']);
+    });
   }
-
-}
+}  
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { AuthService } from '../core/services/auth.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  Nombre: string;
+  status: string;
+  rol: string;
+  tokenUser: string;
+
   constructor(
+    private authService: AuthService,
+    private router: Router,
+    private cookieService: CookieService,
     private navCtrl: NavController,
     private ToastCtrl: ToastController // Inyecta aquí el ToastController
   ) {}
@@ -33,5 +45,31 @@ export class HomePage {
     });
 
     await toast.present();
+  }
+
+  ngOnInit(): void {
+    const token = this.cookieService.get('token');
+    if (!token) {
+      this.router.navigate(['']);
+      return;
+    }
+    this.authService.user().subscribe(user => {
+      if (user && user.user_info) {
+        this.rol = user.user_info.rol; // Asignamos el valor de rol correctamente
+        this.tokenUser = user.user_info.token;
+        console.log(this.rol);
+        
+        if (this.rol !== 'Usuario' &&  this.tokenUser === "") { // Verificamos el valor de rol correctamente
+          this.router.navigate(['']);
+        }
+      } else {
+        console.error('user_info no está definido', user.user_info);
+        
+        this.router.navigate(['']);
+      }
+    }, error => {
+      console.error('Error obteniendo la información del usuario:', error);
+      this.router.navigate(['']);
+    });
   }
 }
