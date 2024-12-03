21ca83f6e83b41daa673dfec88d3ced45f057a97
ErickTDW51 <erickmi01x@gmail.com>
2024-12-02 19:38:53 -0700

    Cookie-service

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  public login(usuario: string, password: string): Observable<any> {
+
+    return this.http.post(environment.url + '/login', { usuario, password })
+
+  }
+  public user(): Observable<any> {
+    return this.http.get(environment.url + '/get-user', {});
+  }
+
+  public registro(usuario:string, nombre:string, email:string, password:string, rol:string): Observable<any>{
+    return this.http.post(environment.url + '/register',{usuario, nombre, email, password, rol});
+  }
+
 }
